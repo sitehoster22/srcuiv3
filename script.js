fb23a103572abb0eed20d76b369399472a249ac0
nightfallenxyz <160358800+nightfallenxyz@users.noreply.github.com>
2025-05-14 14:26:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function handlePlay(id, mediaType) {
 }
 
 function playMovie(tmdbId) {
-    const embedUrl = `https://vidsrc.su/embed/movie/${tmdbId}`;
+    const embedUrl = `http://87.106.101.66:6173/embed/movie/${tmdbId}`;
     document.getElementById('player').innerHTML = `
         <iframe src="${embedUrl}" allowfullscreen></iframe>
     `;
@@ -141,7 +141,7 @@ function playTVShow(tmdbId) {
 }
 
 function updatePlayer(tmdbId, season, episode) {
-    const embedUrl = `https://vidsrc.su/embed/tv/${tmdbId}/${season}/${episode}`;
+    const embedUrl = `http://87.106.101.66:6173/embed/tv/${tmdbId}/${season}/${episode}`;
     document.getElementById('player').innerHTML = `
         <iframe src="${embedUrl}" allowfullscreen></iframe>
     `;
